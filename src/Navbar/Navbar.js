7d8fa9e1ed938f38f2370c496feb105cd120c38e
DannyCoop <cooperdaniel5141@gmail.com>
2020-11-15 15:32:14 -0500

    Fixed and finished  navbar now starting to work on the  body

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -3,14 +3,23 @@ import './Navbar.css'
 
 function Navbar (props) {
     return(
-        <nav>
-            <h1>logo</h1>
-            <ul>
-                <li className="navItems">Home</li>
-                <li className="navItems">Blog</li>
-                <li className="navItems">Github</li>
-            </ul>
-        </nav>
+        <div>
+            <nav className = "navBar">
+                <h1>Coop's Place</h1>
+                <ul>
+                    <li>
+                        <a className="navItems" href="https://www.linkedin.com/notifications/">Home</a>
+                    </li>
+                    <li>
+                        <a className="navItems" href="https://cooperdaniel5141.medium.com/">Blog</a>
+                    </li>
+                    <li>
+                        <a className="navItems" href="https://github.com/DannyCoop">Github</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        
     )
 }
 
