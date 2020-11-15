bd2b1d5091029c123cecdf450612d45e64f8d0bd
DannyCoop <cooperdaniel5141@gmail.com>
2020-11-15 18:07:25 -0500

    adding Project folder and projectCard componet

--- src/About/About.js
+++ src/About/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './About.css'
+import ProjectCard from '../Projects/ProjectCard'
 
 
 function About (props) {
@@ -7,6 +8,9 @@ function About (props) {
         <div>
             <h2>Welcome to Coop's place</h2><br/>
             <p className="AboutMe">Hey everyone I'm a recent bootcamp grad! My main focus is Full stack web development. <br/> A little bit about me besides my bootcamp I also went to Fullsail University and a bachelor's degree in Game Design.</p>
+            <div className="Projects">
+                <ProjectCard/>
+            </div>
         </div>
         
     )
---
+++ src/Projects/ProjectCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ProjectCard () {
+    return(
+        <div>
+            <img src="Link here"/>
+            <h2>Title</h2>
+            <a href="link.com">Demo</a>
+            <a href="link.com">Gihub</a>
+        </div>
+    )
+}
+
+export default ProjectCard
