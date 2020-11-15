dcd10b5f10901be9de14e447161a1475bb83dde4
DannyCoop <cooperdaniel5141@gmail.com>
2020-11-15 18:16:32 -0500

    Added the Footer componet

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer () {
+    return(
+        <div>
+            Contact me at:
+            <a href="linked.com"><img src="icon.png"></img></a>
+            <a href="Cooperdaniel5141@gmail.com.com"><img src="icon.png"></img></a>
+        </div>
+    )
+}
+
+export default Footer
