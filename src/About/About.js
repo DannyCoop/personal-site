84a640636edbf207e302c24d3948eca347caa87f
DannyCoop <cooperdaniel5141@gmail.com>
2020-11-15 17:05:26 -0500

    Added an about component and added a little about bio

---
+++ src/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './About.css'
+
+
+function About (props) {
+    return(
+        <div>
+            <h2>Welcome to Coop's place</h2><br/>
+            <p className="AboutMe">Hey everyone I'm a recent bootcamp grad! My main focus is Full stack web development. <br/> A little bit about me besides my bootcamp I also went to Fullsail University and a bachelor's degree in Game Design.</p>
+        </div>
+        
+    )
+}
+
+
+export default About
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './Navbar/Navbar'
+import About from './About/About'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Navbar/>
+        <Navbar/><br/>
+        <About/>
     </div>
   );
 }
