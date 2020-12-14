780ff7db123a7a9ecafd4973c388e3b21fb20c77
DannyCoop <cooperdaniel5141@gmail.com>
2020-12-13 23:50:21 -0500

    had to swap the comonet being displayed here to the container I also gave the cads a background color so it's not see through any more

--- src/About/About.js
+++ src/About/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './About.css'
-import ProjectCard from '../Projects/ProjectCard'
+import CardContainer from '../Projects/CardContainer.js'
 
 
 function About (props) {
@@ -9,7 +9,7 @@ function About (props) {
             <h2>Welcome to Coop's place</h2><br/>
             <p className="AboutMe">Hey everyone I'm a recent bootcamp grad! My main focus is Full stack web development. <br/> A little bit about me besides my bootcamp I also went to Fullsail University and a bachelor's degree in Game Design.</p>
             <div className="Projects">
-                <ProjectCard/>
+                <CardContainer/>
             </div>
         </div>
         
