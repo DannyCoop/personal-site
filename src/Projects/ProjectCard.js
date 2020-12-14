d95c7d503477cf07d4ed7f29bba99a2179d59fbf
DannyCoop <cooperdaniel5141@gmail.com>
2020-12-13 23:27:11 -0500

    changed the template for a project in to one of my projects

--- src/Projects/ProjectCard.js
+++ src/Projects/ProjectCard.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import './ProjectCard.css'
 
 function ProjectCard () {
     return(
-        <div>
-            <img src="Link here"/>
-            <h2>Title</h2>
-            <a href="link.com">Demo</a>
+        <div className="projectCard">
+            <img src="https://cdn.discordapp.com/attachments/299682345014001675/787896538739376148/mq2.png"/>
+            <h2>Family Center</h2>
+            <span>
+
+            <a href="https://youtu.be/rqC-9KRZFGM">Demo </a>
             <a href="link.com">Gihub</a>
+            </span>
         </div>
     )
 }
