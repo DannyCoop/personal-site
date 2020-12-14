2a7e55f67be66910d5223e48b39a9a7b9cd3ce9b
DannyCoop <cooperdaniel5141@gmail.com>
2020-12-13 23:47:32 -0500

    added a container to hold the cards

---
+++ src/Projects/CardContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProjectCard from './ProjectCard'
+import './CardContainer.css'
+
+function CardContainer(){
+    return(
+        <div className="cards">
+            <ProjectCard/>
+        </div>
+    )
+}
+
+export default CardContainer
