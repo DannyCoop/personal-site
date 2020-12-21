b21496df57adc9ba06532b9ff372978910c9d8cd
DannyCoop <cooperdaniel5141@gmail.com>
2020-12-20 23:52:51 -0500

    puting skills container in to the about component

--- src/About/About.js
+++ src/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './About.css'
 import CardContainer from '../Projects/CardContainer.js'
+import SkillContainer from '../SkillCard/skillContainers'
 
 
 function About (props) {
@@ -10,6 +11,9 @@ function About (props) {
             <p className="AboutMe">Hey everyone I'm a recent bootcamp grad! My main focus is Full stack web development. <br/> A little bit about me besides my bootcamp I also went to Fullsail University and a bachelor's degree in Game Design.</p>
             <div className="Projects">
                 <CardContainer/>
+            </div>
+            <div className="Skills">
+                <SkillContainer/>
             </div>
         </div>
         
