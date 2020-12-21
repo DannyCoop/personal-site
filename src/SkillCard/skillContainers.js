e5e501e47cbdf90f4928779790b27c001c450706
DannyCoop <cooperdaniel5141@gmail.com>
2020-12-20 23:59:22 -0500

    trying a test out

--- src/SkillCard/skillContainers.js
+++ src/SkillCard/skillContainers.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
+import SkillCards from './SkillCards';
 
 function SkillContainer(){
+    const [skills, setSkills] = useState(["JavaScript", "ruby"])
+
+    const showSkills = () =>{
+    }
+
     return(
         <div>
-
+            <SkillCards/>
         </div>
     )
 }
