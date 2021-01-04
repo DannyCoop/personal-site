65547d2c462d35361ee195b2075092b06d6c65cf
DannyCoop <cooperdaniel5141@gmail.com>
2021-01-03 23:57:24 -0500

    added the backgorund to the new sections i added

--- src/About/About.js
+++ src/About/About.js
@@ -10,12 +10,12 @@ function About (props) {
         <div>
             <h2>Welcome to Coop's place</h2><br/>
             <p className="AboutMe">Hey everyone I'm a recent bootcamp grad! My main focus is Full stack web development. <br/> A little bit about me besides my bootcamp I also went to Fullsail University and a bachelor's degree in Game Design.</p>
-            <div>
+            <div className="current_project">
                 <h3>Current project</h3>
                 <h4>Tribe</h4>
                 <textarea>Tribe is the project that I've been working on lately.Tribe is a reboot of my final project Family center that I was able to get some friends to help me with.</textarea>
             </div>
-            <div>
+            <div className="interest">
                 <h3>interest </h3>
                 <textarea>I'm really in to games and hanging out with my friends. I was able to join  a hackathon and i plan on doing more.</textarea>
             </div>
