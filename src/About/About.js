9259fa992730620a82913cfd889ef0f3ec435e6d
DannyCoop <cooperdaniel5141@gmail.com>
2021-01-10 23:26:31 -0500

    added some line breaks to help sperate the text

--- src/About/About.js
+++ src/About/About.js
@@ -10,7 +10,6 @@ function About (props) {
         <div>
             <h2>Welcome to Coop's place</h2><br/>
             <p className="AboutMe">Hey everyone I'm a recent bootcamp grad! My main focus is Full stack web development. <br/> A little bit about me besides my bootcamp I also went to Fullsail University and a bachelor's degree in Game Design.</p>
-            <br/>
             <div className="current_project">
                 <h3>Current project</h3>
                 <h4>Tribe</h4>
