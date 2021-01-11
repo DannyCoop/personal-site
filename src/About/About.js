5427fe6b6f0e1cc0aa044fae032f64791093d049
DannyCoop <cooperdaniel5141@gmail.com>
2021-01-10 23:19:50 -0500

    fixed the tags for the about me sections

--- src/About/About.js
+++ src/About/About.js
@@ -10,15 +10,16 @@ function About (props) {
         <div>
             <h2>Welcome to Coop's place</h2><br/>
             <p className="AboutMe">Hey everyone I'm a recent bootcamp grad! My main focus is Full stack web development. <br/> A little bit about me besides my bootcamp I also went to Fullsail University and a bachelor's degree in Game Design.</p>
+            <br/>
             <div className="current_project">
                 <h3>Current project</h3>
                 <h4>Tribe</h4>
-                <textarea>Tribe is the project that I've been working on lately.Tribe is a reboot of my final project Family center that I was able to get some friends to help me with.</textarea>
-            </div>
+                <p>Tribe is the project that I've been working on lately.Tribe is a reboot of my final project Family center that I was able to get some friends to help me with.</p>
+            </div><br/>
             <div className="interest">
                 <h3>interest </h3>
-                <textarea>I'm really in to games and hanging out with my friends. I was able to join  a hackathon and i plan on doing more.</textarea>
-            </div>
+                <p>I'm really in to games and hanging out with my friends. I was able to join  a hackathon and i plan on doing more.</p>
+            </div><br/>
             <div className="Projects">
                 <CardContainer/>
             </div>
