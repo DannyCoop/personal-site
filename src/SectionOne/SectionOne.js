5e3982167c8fe50d066ab494fad8b57dcfca5e35
DannyCoop <cooperdaniel5141@gmail.com>
2021-01-31 22:36:33 -0500

    Added the css file for the Section one

--- src/SectionOne/SectionOne.js
+++ src/SectionOne/SectionOne.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function SectionOne(){
     return(
-        <div>
-            
+        <div className="SectionOne">
+            <div className="SectionOne-Picture">
+                
+            </div>
+            <div className="SectionOne-Picture">
+                
+            </div>
         </div>
     )
 }
